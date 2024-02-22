053ac0eb2f371733af3c795034f3d11091e9e550
triandre813 <159310616+triandre813@users.noreply.github.com>
2024-02-22 18:43:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Set myCurrentAge to 53
+# Set currentYear to 2024
+#Set myNewAge to myCurrentAge + (2050 - currentYear)
+#Output "My Current Age is " + myCurrentAge converted to string
+#Output "I will be " + myNewAge converted to string = " in 2050."
+
+myCurrentAge = 53
+currentYear = 2024
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
